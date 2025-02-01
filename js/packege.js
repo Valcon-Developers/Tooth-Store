db3d3558131b81bcf40082c81391aa0d6fbbb363
~Developer <mgwaham167@gmail.com>
2025-02-02 01:01:08 +0200

    Update packege.js

--- js/packege.js
+++ js/packege.js
@@ -3,6 +3,7 @@ let packge=[
     // {id:2,title:"Semester 2",price:15000,source:"imgs/Box.jpg",description: "packges/semester2",order:""},
     // {id:3,title:"Semester 3",price:15000,source:"imgs/Box.jpg",description: "packges/semester2",order:"التوصيل مجانا"},
     {id:4,title:"Semester 4",price:6300 ,source:"imgs/Box.jpg",description: "packges/semester4",order:"التوصيل مجانا"},
+    {id:4,title:"عرض الخمس صحاب",price:4900,price2:"5900 EGP",source:"imgs/Box.jpg",description: "packges/semester4",order:""},
     // {id:5,title:"Semester 5",price:10000,source:"imgs/Box.jpg",description: "packges/semester4",order:"التوصيل مجانا"},
     
     
@@ -14,12 +15,13 @@ function card(i){
     return(
         `
         
-        <div class="bg-light card text-dark card-product search-card mx-auto my-5" style="border:solid 2px #17a2b8; display: grid; grid-template-columns: repeat(auto-fill, minmax(200px, 1fr)); grid-gap: 20px; max-height: 390px;">
+      <div class="bg-light card text-dark card-product search-card mx-auto my-5" style="border:solid 2px #17a2b8; display: grid; grid-template-columns: repeat(auto-fill, minmax(200px, 1fr)); grid-gap: 20px; max-height: 390px;">
             <div class="card-img bg-light" onclick="showDetailes(${i})" style=" overflow: hidden;border-radius:30px">
                 <img src=${packge[i].source} alt=""  style=" overflow: hidden;object-fit: cover;">
             </div>
             <div class="card-body bg-light" onclick="showDetailes(${i})" style="display: flex; flex-direction: column; justify-content: space-between;"> 
                 <div class="card-title h5 search-title">${packge[i].title}</div>
+                <h5 style="text-decoration: line-through;">${packge[i].price2}</h5>
                 <div class="card-text h5">${packge[i].price} EGP</div>
                 <h5>${packge[i].order}</h5>
             </div>
